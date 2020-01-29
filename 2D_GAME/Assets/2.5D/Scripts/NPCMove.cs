ccae3d527d6be6483e983c9a4b55807db67055b0
mikzzzik <mikzzzik1@Gmail.com>
2020-01-29 02:59:16 +0200

    Update #2

---
+++ 2D_GAME/Assets/2.5D/Scripts/NPCMove.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMove : MonoBehaviour
+{
+    public GameObject _player;
+    public float _speed;
+    public bool _canmove;
+    public Transform[] _point;
+    public int _rPoint;
+    public float _Timer;
+    public float _StartTime;
+    public Animator _mAnim;
+
+    void Start()
+    {
+        _canmove = true;
+        _Timer = _StartTime;
+        _rPoint = Random.Range(0, _point.Length);
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _mAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_canmove)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _point[_rPoint].position, _speed * Time.deltaTime);
+        }
+      
+        Debug.Log(transform.position);
+        Debug.Log(_point[_rPoint].position);
+        var x = transform.position.x - _point[_rPoint].position.x;
+        var y = transform.position.y - _point[_rPoint].position.y;
+
+        if (x > 0)
+        {
+            _mAnim.SetBool("Right", false);
+            _mAnim.SetBool("Left", true);
+        }
+        else
+        {
+           _mAnim.SetBool("Right", true);
+            _mAnim.SetBool("Left", false);
+        }
+            
+        if (Vector2.Distance(transform.position, _point[_rPoint].position) < 0.2f)
+        {
+            if(_Timer <= 0)
+            {
+                _rPoint = Random.Range(0, _point.Length);
+                _Timer = _StartTime;
+            }
+            else
+            {
+                _Timer -= Time.deltaTime;
+            }
+        }
+        
+    }
+}
---
+++ 2D_GAME/Assets/2.5D/Scripts/NPC_Gray_Dialog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Gray_Dialog : MonoBehaviour
+{
+    public NPCMove _npc;
+
+    public GameObject _player;
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _npc = gameObject.GetComponent<NPCMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, _player.transform.position) < 0.5f)
+        {
+            if (Input.GetAxisRaw("Open") == 1)
+            {
+                _npc._canmove = false;
+            }
+          
+            
+
+        }
+    }
+}
